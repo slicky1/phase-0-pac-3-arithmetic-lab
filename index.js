f1738f8722040144970fc14c2caafbb702f785ae
slicky1 <okunonke@gmail.com>
2021-03-14 14:21:07 -0400

    Assigments Completed

---
+++ index.js
@@ -0,0 +1,39 @@
+function add (a, b){
+    return a+b
+   }
+   console.log (add(1,3));
+
+function subtract (a, b){
+    return a-b
+   }
+   console.log (subtract(9,3));
+
+   function multiply (a, b){
+    return a*b
+   }
+   console.log (multiply(7,3));
+
+   function divide (a, b){
+    return a/b
+   }
+   console.log (divide(9,3));
+
+   function increment (a){
+    return a + 1 
+   }
+   console.log (increment(4));
+   
+   function decrement(a){
+    return a - 1 
+   }
+   console.log (decrement(10));
+   
+   function makeInt(string) {
+       return parseInt(string, 10)
+       
+   }
+
+   function preserveDecimal(string) {
+       return parseFloat(string)
+   }
+
